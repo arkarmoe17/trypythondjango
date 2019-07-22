488ece3d0c34edf2a5c19272bd02e1a85d11be1d
Arkar Moe <arkarmoe1@mytel.com.mm>
2019-07-22 17:35:03 +0630

    html pages

--- pages/views.py
+++ pages/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")  # string of html code
+def home_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Hello World</h1>")  # string of html code
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
 
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")  # contact view
-
-
-def about_view(*args, **kwargs):
-    return HttpResponse("<h1>About Page</h1>")  # about view
+def about_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>About Page</h1>")  # about view
+    return render(request, "about.html", {})
--- trydjango/settings.py
+++ trydjango/settings.py
@@ -59,7 +59,8 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': ['/pycharm/trydjango/src/templates/'],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
